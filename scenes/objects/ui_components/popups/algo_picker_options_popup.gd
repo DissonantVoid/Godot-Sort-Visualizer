ebdd452d3d350bd76bf020d2f2f99adb5f6128ff
DissonantVoid <arandomdude75@gmail.com>
2022-12-31 12:58:44 +0100

    popups!

--- scenes/objects/ui_components/popups/algo_picker_options_popup.gd
+++ scenes/objects/ui_components/popups/algo_picker_options_popup.gd
@@ -1,2 +1,9 @@
 extends "res://scenes/objects/ui_components/popups/popup_base.gd"
 
+onready var _step_time_box : SpinBox = $Content/VBoxContainer/GridContainer/SpinBox
+
+
+# override and do error checking
+func _on_ok_pressed():
+	emit_signal("ok", {"step_time":_step_time_box.value})
+	queue_free()
--- scenes/objects/ui_components/popups/popup_base.gd
+++ scenes/objects/ui_components/popups/popup_base.gd
@@ -3,6 +3,12 @@ extends Control
 signal ok(data) # data : dictionary
 signal cancel
 
+onready var _error_panel : PanelContainer = $Error
+onready var _error_txt : Label = $Error/HBoxContainer/Label
+
+const _err_show_time : float = 0.3
+
+
 func _on_background_gui_input(event):
 	if event is InputEventMouseButton && event.pressed && event.button_index == BUTTON_LEFT:
 		emit_signal("cancel")
@@ -10,10 +16,19 @@ func _on_background_gui_input(event):
 
 # override and do error checking
 func _on_ok_pressed():
-	emit_signal("cancel")
+	emit_signal("ok", {})
 	queue_free()
 
 # override
 func _on_cancel_pressed():
 	emit_signal("cancel")
 	queue_free()
+
+func _on_close_error_pressed():
+	_error_panel.hide()
+
+func _error(text : String):
+	var tween : SceneTreeTween = get_tree().create_tween()
+	tween.tween_property(_error_panel, "modulate:a", 1.0, _err_show_time).from(0.0)
+	_error_txt.text = "Error: " + text
+	_error_panel.show()
