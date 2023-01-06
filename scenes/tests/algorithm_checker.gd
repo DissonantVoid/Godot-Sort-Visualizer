c70d8e560d3d81b766bbb3107cf8704852feff1c
DissonantVoid <arandomdude75@gmail.com>
2023-01-06 13:09:21 +0100

    algorithms checker improvements

--- scenes/tests/algorithm_checker.gd
+++ scenes/tests/algorithm_checker.gd
@@ -12,10 +12,16 @@ var _selected_algorithm_name : String
 var _use_next_step_func : bool = true
 var _array_size : int = 10
 var _allow_duplicates : bool = false
+var _trace_steps : bool = false
 
-const _max_printable_array_size : int = 30 # _current_arr is printed to console as long as it's smaller than this
-var _current_arr : Array
 var _rng : RandomNumberGenerator = RandomNumberGenerator.new()
+const _max_printable_array_size : int = 30 # _current_arr is printed to console as long as it's smaller than this
+var _current_input : Array
+var _prev_report_end_idx_cache : int = -1
+
+const _bad_color : String = "#f83f3f"
+const _good_color : String = "#92e229"
+
 
 func _ready():
 	_rng.randomize()
@@ -58,25 +64,29 @@ func _on_array_size_changed(value : float):
 func _on_allow_duplicates_toggled(button_pressed : bool):
 	_allow_duplicates = button_pressed
 
+func _on_trace_steps_toggled(button_pressed : bool):
+	_trace_steps = button_pressed
+
 # TODO: what a mess
 func _on_run_test_pressed():
 	_run_btn.disabled = true
+	_prev_report_end_idx_cache = _console.bbcode_text.length()
 	
 	# setup
-	_current_arr.resize(_array_size)
+	_current_input.resize(_array_size)
 	if _allow_duplicates:
 		for i in _array_size:
-			_current_arr[i] = _rng.randi_range(0, 100)
+			_current_input[i] = _rng.randi_range(0, 100)
 	else:
-		for i in _array_size: _current_arr[i] = i
+		for i in _array_size: _current_input[i] = i
 		
 		for i in _array_size:
 			var rand_idx : int = _rng.randi_range(0, _array_size-1)
-			var temp_i : int = _current_arr[i]
-			_current_arr[i] = _current_arr[rand_idx]
-			_current_arr[rand_idx] = temp_i
+			var temp_i : int = _current_input[i]
+			_current_input[i] = _current_input[rand_idx]
+			_current_input[rand_idx] = temp_i
 	
-	#print("starting arr: " + str(_current_arr)) # TEMP
+	
 	var sorter_object = load(AlgorithmsTracker.get_dict()[_selected_algorithm_name]).new()
 	sorter_object.setup(_array_size, funcref(self, "_is_bigger"))
 	
@@ -89,59 +99,64 @@ func _on_run_test_pressed():
 	
 	_print_console("using array of size " + str(_array_size) +
 					(", duplicates allowed" if _allow_duplicates else ", no duplicates") )
+	if _array_size < _max_printable_array_size:
+		_print_console("input array: " + str(_current_input))
 	_print_console("")
 	
-	var original_array : Array = _current_arr.duplicate()
+	var original_array : Array = _current_input.duplicate()
 	var has_errors : bool = false
 	if _use_next_step_func: # next_step()
 		var iterations : int = 0
 		while true:
 			var result : Dictionary = sorter_object.next_step()
 			if result.has("done") == false:
-				_print_console("sorter.next_step() return doesn't contain 'done' entry", true)
+				_print_console("sorter.next_step() return doesn't contain 'done' entry", _bad_color)
 				has_errors = true
 				break
 			if result["done"]: break
 				
 			else:
 				if result.has("indexes") == false:
-					_print_console("sorter.next_step() return doesn't contain 'indexes' entry", true)
+					_print_console("sorter.next_step() return doesn't contain 'indexes' entry", _bad_color)
 					has_errors = true
 					break
 				elif result["indexes"].size() != 2:
-					_print_console("sorter.next_step() 'indexes' return contains more or less than 2 entries", true)
+					_print_console("sorter.next_step() 'indexes' return contains more or less than 2 entries", _bad_color)
 					has_errors = true
 					break
 				
-				var temp_idx1 : int = _current_arr[result["indexes"][0]]
-				_current_arr[result["indexes"][0]] = _current_arr[result["indexes"][1]]
-				_current_arr[result["indexes"][1]] = temp_idx1
-				#print(_current_arr) # TEMP
+				var temp_idx1 : int = _current_input[result["indexes"][0]]
+				_current_input[result["indexes"][0]] = _current_input[result["indexes"][1]]
+				_current_input[result["indexes"][1]] = temp_idx1
+				
+				if _trace_steps:
+					_print_console("step " + str(iterations) + ": " + str(_current_input))
+				
 				iterations += 1
 		if has_errors:
-			_print_console("sorter.next_step() encountered an error after " + str(iterations) + " iterations", true)
+			_print_console("sorter.next_step() encountered an error after " + str(iterations) + " iterations", _bad_color)
 		else:
 			_print_console("sorter.next_step() finished after " + str(iterations) + " iterations")
 	else: # skip_to_last_step()
 		var new_indexes : Array = sorter_object.skip_to_last_step()
-		if new_indexes.size() != _current_arr.size():
-			_print_console("returned array size doesn't match input array size", true)
+		if new_indexes.size() != _current_input.size():
+			_print_console("returned array size doesn't match input array size", _bad_color)
 			has_errors = true
 		else:
 			var new_current_arr : Array
 			new_current_arr.resize(new_indexes.size())
 			
 			for i in new_indexes.size():
-				new_current_arr[i] = _current_arr[new_indexes[i]]
+				new_current_arr[i] = _current_input[new_indexes[i]]
 			
-			_current_arr = new_current_arr
+			_current_input = new_current_arr
 	
 	
 	if has_errors == false:
 		# check new returned array validity
-		for i in range(1, _current_arr.size()):
-			if _current_arr[i] < _current_arr[i-1]:
-				_print_console("sorted array is not sorted right", true)
+		for i in range(1, _current_input.size()):
+			if _current_input[i] < _current_input[i-1]:
+				_print_console("sorted array is not sorted right", _bad_color)
 				has_errors = true
 				break
 		
@@ -149,7 +164,7 @@ func _on_run_test_pressed():
 			_print_console("input array:")
 			_print_console(str(original_array))
 			_print_console("result array:")
-			_print_console(str(_current_arr))
+			_print_console(str(_current_input))
 		else:
 			_print_console(
 				"array is too big to print, make array size smaller than " +
@@ -157,24 +172,33 @@ func _on_run_test_pressed():
 				" if you want to see its content"
 			)
 	
-	_print_console("sorting finished " + 
-					("with errors" if has_errors else "successfully"),
-					has_errors
-	)
+	if has_errors:
+		_print_console("sorting finished with errors", _bad_color)
+	else:
+		_print_console("sorting finished successfully", _good_color)
+		
 	_print_console("--------------------------------")
 	
 	_run_btn.disabled = false
 
+func _on_delete_old_reports_pressed():
+	if _prev_report_end_idx_cache == -1: return
+	
+	_console.bbcode_text = _console.bbcode_text.substr(_prev_report_end_idx_cache)
+	_prev_report_end_idx_cache = -1
+
+
 func _is_bigger(idx1 : int, idx2 : int) -> bool:
-	return _current_arr[idx1] > _current_arr[idx2]
+	return _current_input[idx1] > _current_input[idx2]
 
 func _toggle_checkbox(target : CheckBox, all_boxes : Array):
 	for box in all_boxes:
 			if box != target:
 				box.set_pressed_no_signal(false)
 
-func _print_console(text : String, is_err : bool = false):
-	if is_err:
-		text = "[color=red]" + text + "[/color]"
-	
-	_console.bbcode_text += text + '\n'
+func _print_console(text : String, color_hex : String = "#fff"):
+	# TODO: we can't make color_hex of type Color, bacause the color tag
+	#       in bbcode doesn't support rgb, I should send an issue about this
+	#       so that either Color can return a hex code or the color tag can accept rgb
+	_console.bbcode_text += "[color=" + color_hex + "]" + text + "[/color]" + '\n'
+
