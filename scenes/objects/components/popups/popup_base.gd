f7b9ea4033da384e941b109bdb6817bb2cef22a3
Dissonant Void <arandomdude75@gmail.com>
2023-01-16 19:39:19 +0000

    added fade-in animation to popups
    attention to details? feature creep? boredom? we may never know

--- scenes/objects/components/popups/popup_base.gd
+++ scenes/objects/components/popups/popup_base.gd
@@ -6,8 +6,30 @@ signal cancel
 onready var _error_panel : MarginContainer = $Error
 onready var _error_txt : Label = $Error/Error/HBoxContainer/Label
 
+const _fade_in_time : float = 0.08
+const _max_fade_depth : int = 3 # fade in nodes as long as they're not this deep in tree
 const _err_show_time : float = 0.3
 
+
+func _ready():
+	# recursively animate elements showing up
+	var content_container : PanelContainer = $Content
+	var tween : SceneTreeTween = get_tree().create_tween()
+	yield(get_tree(),"idle_frame") # wait in case derived classes add nodes in _ready
+	for child in content_container.get_children():
+		_tween_children(child, tween, 0)
+
+func _tween_children(node : CanvasItem, tween : SceneTreeTween, depth : int):
+	if depth <= _max_fade_depth:
+		for child in node.get_children():
+			if child is CanvasItem:
+				_tween_children(child, tween, depth+1)
+		
+		node.self_modulate.a = 0
+		tween.tween_property(node, "self_modulate:a", 1.0, _fade_in_time)
+	else:
+		node.modulate.a = 0
+		tween.tween_property(node, "modulate:a", 1.0, _fade_in_time)
 
 func _on_background_gui_input(event):
 	if event is InputEventMouseButton && event.pressed && event.button_index == BUTTON_LEFT:
